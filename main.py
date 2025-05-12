15259c70e5def4737c287678efe01dd82c7cdf8d
Jonathan Smith <jonathan@peoplesized.com>
2025-05-12 18:52:21 +0200

    ok again

--- main.py
+++ main.py
@@ -5,19 +5,9 @@ from prefect.deployments import run_deployment
 
 
 @flow
-def the_flow() -> None:
+def flow_sees_subflow(n: int = 5) -> None:
     logger = get_run_logger()
-    logger.info("hello from the_flow")
-    for i in range(5):
-        logger.info(f"iteration {i}")
-        value = a_task()
-        logger.info(f"got value from a_task = {value}")
-
-
-@flow
-def subflow(n: int = 5) -> None:
-    logger = get_run_logger()
-    logger.info(f"hello from the_another_flow, n = {n}")
+    logger.info(f"hello from flow_sees_subflow, n = {n}")
     for i in range(n):
         logger.info(f"iteration {i}")
         value = a_task()
@@ -29,23 +19,23 @@ def subflow(n: int = 5) -> None:
 def flow_sees_flow() -> None:
     logger = get_run_logger()
     logger.info("hello from flow_sees_flow")
-    logger.info("calling subflow/subflow")
+    logger.info("calling flow_sees_subflow/subflow")
     i = 1
-    flow_run = run_deployment(name="subflow/subflow", parameters={"n": i})
+    flow_run = run_deployment(name="flow_sees_subflow/subflow", parameters={"n": i})
     logger.info(f"flow_run = {flow_run}")
-    logger.info("waiting for subflow/subflow to finish?")
+    logger.info("waiting for flow_sees_subflow/subflow to finish?")
     i = i + 1
-    flow_run = run_deployment(name="subflow/subflow", parameters={"n": i})
+    flow_run = run_deployment(name="flow_sees_subflow/subflow", parameters={"n": i})
     logger.info(f"flow_run = {flow_run}")
-    logger.info("waiting for subflow/subflow to finish?")
+    logger.info("waiting for flow_sees_subflow/subflow to finish?")
     i = i + 1
-    flow_run = run_deployment(name="subflow/subflow", parameters={"n": i})
+    flow_run = run_deployment(name="flow_sees_subflow/subflow", parameters={"n": i})
     logger.info(f"flow_run = {flow_run}")
-    logger.info("waiting for subflow/subflow to finish?")
+    logger.info("waiting for flow_sees_subflow/subflow to finish?")
     i = i + 1
-    flow_run = run_deployment(name="subflow/subflow", parameters={"n": i})
+    flow_run = run_deployment(name="flow_sees_subflow/subflow", parameters={"n": i})
     logger.info(f"flow_run = {flow_run}")
-    logger.info("waiting for subflow/subflow to finish?")
+    logger.info("waiting for flow_sees_subflow/subflow to finish?")
 
 
 @task
@@ -54,7 +44,3 @@ def a_task() -> int:
     value = random.randint(1, 100)
     logger.info(f"hello from a_task. got value = {value}")
     return value
-
-
-if __name__ == "__main__":
-    the_flow.serve()
